fc58012123337d333f60491ba067aba3e1490120
Cornel Andreev <hugronaphor@gmail.com>
2015-11-12 10:15:00 +0000

    Detect Ad delivery by taboola.

--- modules/taboola_dfp_native/js/taboola_dfp_native.js
+++ modules/taboola_dfp_native/js/taboola_dfp_native.js
@@ -9,8 +9,23 @@
         // Append the rendered Dfp slot to specified element.
         appendDfp: function (tags) {
           $.each(tags, function (index, item) {
-            position = item.position != undefined && item.position > 0 ? item.position : 0;
+            var slotName = item.slotMachinename,
+              position = (item.position != undefined && item.position > 0) ? item.position : 0;
             $(item.selector).eq(position).before(item.renderedDfp);
+            googletag.pubads().addEventListener('slotRenderEnded', function (e) {
+              // Detect if the Ad is actually delivered.
+              if ((e.slot == googletag.slots[slotName]) && (e.isEmpty === false)) {
+                // Hide the last element from selector if this this
+                // is requested on context reaction.
+                if (item.last_el_hide != undefined && item.position > 0) {
+                  $(item.selector).last().addClass('element-invisible');
+                }
+              }
+              // Otherwise, make sure to hide it.
+              else {
+                $('#' + item.slotDfpWrapperID).addClass('element-invisible');
+              }
+            });
           });
         },
         // appendDfp initialization function.
