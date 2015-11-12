4b5b57010e2b1ae48ad38bb48ad216fe94ff6457
Cornel Andreev <hugronaphor@gmail.com>
2015-11-12 16:05:13 +0000

    Allow modules to alter the configuration object.

---
+++ modules/taboola_dfp_native/taboola_dfp_native.api.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Chaos Tool Suite.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter the raw tag object just after it is loaded from the database.
+ *
+ * @param object $tag
+ */
+
+/**
+ * Alter the Taboola native configuration tags.
+ */
+function hook_taboola_taboola_dfp_native_tags_alter(&$tags) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
