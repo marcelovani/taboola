6c83ec3e04be6a310ee937fe1569ccc4e697a77f
Tariq Ghori <tariq.inaam@gmail.com>
2015-11-19 18:09:42 +0000

    multiple selector functionality added

--- modules/taboola_dfp_native/js/taboola_dfp_native.js
+++ modules/taboola_dfp_native/js/taboola_dfp_native.js
@@ -10,7 +10,13 @@
         appendDfp: function (tags) {
           $.each(tags, function (index, item) {
             position = item.position != undefined && item.position > 0 ? item.position : 0;
-            $(item.selector).eq(position).before(item.renderedDfp);
+            // loop through each selector: if a match found, render dfp and break loop.
+            $.each(item.selector, function (index, itemValue) {
+              if($(itemValue).eq(position).length == 1) {
+                $(itemValue).eq(position).before(item.renderedDfp);
+                return false;
+              }
+            });
           });
         },
         // appendDfp initialization function.
